84602c9a99cc5d55978beb676d101767dcdac1ce
KJ Lee <kunnyjlee@gmail.com>
2025-04-13 12:55:43 -0700

    added backend files

---
+++ bappdat_backend/src/test/java/com/example/bappdat/BappdatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bappdat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BappdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
